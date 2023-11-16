4463e58dc729dd403b65669783e5f91625a63c35
DESKTOP-RLUVES5\Maedeh Tehrani <maede.alirezaei@gmail.com>
2023-11-16 16:17:51 +0100

    Change style

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
-import { Box, Tab, Tabs } from '@mui/material'
-import './App.css'
-import { useState } from 'react';
-import { TabPanel, a11yProps } from './components/TabPanel';
+import { Box, Tab, Tabs } from "@mui/material";
+import { useEffect, useRef, useState } from "react";
+import { TabPanel, a11yProps } from "./components/TabPanel";
 
 function App() {
   const [value, setValue] = useState(0);
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
+
+  const tabsRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = (event: { deltaY: number }) => {
+      console.log(event);
+      if (event.deltaY > 0) {
+        setValue((prev) => prev + 1);
+      } else {
+        setValue((prev) => prev - 1);
+      }
+    };
+
+    window.addEventListener("wheel", handleScroll);
+
+    return () => {
+      window.removeEventListener("wheel", handleScroll);
+    };
+  }, []);
   return (
-    <Box
-      sx={{  bgcolor: 'background.paper', display: 'flex', height: 224 , width:'100%'}}
-    >
+    <Box sx={{ bgcolor: "background.paper", display: "flex", width: "100%" }}>
       <Tabs
+        ref={tabsRef}
         orientation="vertical"
         value={value}
         onChange={handleChange}
         variant="scrollable"
         scrollButtons={false}
         aria-label="scrollable prevent tabs example"
-        sx={{ borderRight: 1, borderColor: 'divider' }}
+        sx={{ borderRight: 1, borderColor: "divider" }}
       >
         <Tab label="Item One" {...a11yProps(0)} />
         <Tab label="Item Two" {...a11yProps(1)} />
@@ -51,7 +68,7 @@ function App() {
         Item Seven
       </TabPanel>
     </Box>
-  )
+  );
 }
 
-export default App
+export default App;
